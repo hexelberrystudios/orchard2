e818807fe4892723c96af39ed893b09da9dd9483
Dogleon <yotsubato32@gmail.com>
2017-02-09 15:15:40 +0000

    First pass on data gathering complete

--- src/components/pages/NewItemPageStep2.vue
+++ src/components/pages/NewItemPageStep2.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <app-header></app-header>
-    <h1 class="hxb-u-pdl-1">Time to add a new {{ template.name }}!</h1>
+    <h1 class="hxb-u-pdl-1" v-if="template">Time to add a new {{ template.name }}!</h1>
     <form v-on:submit="addItem" name="new_item" method="POST" action="/new-item" class="hxb-form">
       <text-field id="name" label="Name"></text-field>
       <template v-for="field in template.fields">
@@ -16,32 +16,41 @@
 </template>
 
 <script>
+  import { mapGetters } from 'vuex'
   import AppFooter from '../AppFooter.vue'
   import AppHeader from '../AppHeader.vue'
   import SubmitButton from '../SubmitButton.vue'
   
+  const fetchInitialData = store => {
+    let templateId
+    let searchStr
+    
+    if (typeof window !== 'undefined') {
+      searchStr = window.location.search
+      
+      if (searchStr) {
+        templateId = searchStr.substr(searchStr.lastIndexOf('=' + 1))
+      }
+    }
+    
+    if (templateId) {
+      return store.dispatch('getActiveTemplate', templateId)
+    } else {
+      // @TODO: Show user facing error and redirect them to the home page
+      throw Error('No active template found.')
+    }
+  }
+  
   export default {
     name: 'new-item-page-2',
-    data: function () {
-      return {
-        fields: [],
-        template: {}
-      }
-    },
-    created: function () {
-      // let templateId;
-      // let self = this;
-
-      // get the template id from the url
-      /*
-      hoodie.ready.then(function () {
-        // look through the DB for all the templates
-        hoodie.store.find(templateId)
-          .then((template) => {
-            self.template = template;
-          })
+    prefetch: fetchInitialData,
+    computed: {
+      ...mapGetters({
+        template: 'getActiveTemplate'
       })
-      */
+    },
+    mounted () {
+      fetchInitialData(this.$store)
     },
     methods: {
       addItem: function (e) {
--- src/vuex/actions.js
+++ src/vuex/actions.js
@@ -2,6 +2,45 @@ import request from 'axios'
 
 request.defaults.baseURL = 'http://jsonplaceholder.typicode.com/'
 
+/*
+ * Either wait for hoodie to load (if you're on the client),
+ * or call hoodie directly somehow (if you're on the server) (@TODO),
+ * then call the given function to process data.
+ */
+const runHoodieFn = (commit, state, fn) => {
+  if (typeof window !== 'undefined') {
+    // @TODO understand why the hoodie constant is not available here server side
+    // @TODO remove, just understanding that hoodie needs time to initialize
+    if (typeof hoodie === 'undefined') {
+      console.log('hoodie is not defined yet')
+    }
+    
+    let waitForHoodieToLoad = new Promise(function (resolve, reject) {
+      let intervalId
+      
+      console.log('waiting for hoodie to load')
+      
+      intervalId = setInterval(function () {
+        if (typeof hoodie !== 'undefined') {
+          console.log('hoodie loaded!')
+          
+          clearInterval(intervalId);
+          hoodie.ready.then(function () {
+            fn(resolve, reject)
+          })
+        } else {
+          console.log('hoodie not loaded')
+        }
+      }, 20)
+    })
+    
+    return waitForHoodieToLoad
+  } else {
+    // @TODO: just return an empty object until we have something to show for this
+    return {}
+  }
+}
+
 export const getTopics = ({ commit, state }) => {
   return request.get('posts').then((response) => {
     if (response.statusText === 'OK') {
@@ -13,97 +52,46 @@ export const getTopics = ({ commit, state }) => {
 }
 
 export const getTemplates = ({ commit, state }) => {
-  if (typeof window !== 'undefined') {
-    // @TODO understand why the hoodie constant is not available here server side
-    // @TODO remove, just understanding that hoodie needs time to initialize
-    if (typeof hoodie === 'undefined') {
-      console.log('hoodie is not defined yet')
-    }
-    
-    let waitForHoodieToLoad = new Promise(function (resolve, reject) {
-      let intervalId
-      
-      console.log('waiting for hoodie to load')
-      
-      intervalId = setInterval(function () {
-        if (typeof hoodie !== 'undefined') {
-          console.log('hoodie loaded!')
-          
-          clearInterval(intervalId);
-          hoodie.ready.then(function () {
-            // look through the DB for all the templates
-            hoodie.store.findAll()
-              .then((docs) => {
-                return docs.filter(doc => doc.templateName) // filter out docs that have no templateName field
-              })
-              .then((templateDocs) => {
-                console.log('done loading templates')
-                // update the store with the list of available templates
-                commit('TEMPLATE_LIST', templateDocs)
-                resolve(templateDocs)
-              })
-              .catch((error) => {
-                console.log(error)
-                reject(error)
-              })
-          })
-        } else {
-          console.log('hoodie not loaded')
-        }
-      }, 20)
-    })
-    
-    return waitForHoodieToLoad
-  } else {
-    // just return an empty object until we have something to show for this
-    return {}
+  const findAllTemplates = function (resolve, reject) {
+    // look through the DB for all the templates
+    hoodie.store.findAll()
+      .then((docs) => {
+        return docs.filter(doc => doc.templateName) // filter out docs that have no templateName field
+      })
+      .then((templateDocs) => {
+        console.log('done loading templates')
+        console.log(templateDocs)
+        // update the store with the list of available templates
+        commit('TEMPLATE_LIST', templateDocs)
+        resolve(templateDocs)
+      })
+      .catch((error) => {
+        console.log(error)
+        reject(error)
+      })
   }
+          
+  return runHoodieFn(commit, state, findAllTemplates)
 }
 
 export const getTemplate = ({ commit, state, templateId }) => {
-  console.log(templateId)
-  if (typeof window !== 'undefined') {
-    // @TODO understand why the hoodie constant is not available here server side
-    // @TODO remove, just understanding that hoodie needs time to initialize
-    if (typeof hoodie === 'undefined') {
-      console.log('hoodie is not defined yet')
-    }
-    
-    let waitForHoodieToLoad = new Promise(function (resolve, reject) {
-      let intervalId
-      
-      console.log('waiting for hoodie to load')
-      
-      intervalId = setInterval(function () {
-        if (typeof hoodie !== 'undefined') {
-          console.log('hoodie loaded!')
-          
-          clearInterval(intervalId);
-          hoodie.ready.then(function () {
-            // look through the DB for all the templates
-            hoodie.store.find(templateId)
-              .then((templateDoc) => {
-                console.log('done loading template')
-                // update the store with the list of available templates
-                commit('ACTIVE_TEMPLATE', templateDoc)
-                resolve(templateDoc)
-              })
-              .catch((error) => {
-                console.log(error)
-                reject(error)
-              })
-          })
-        } else {
-          console.log('hoodie not loaded')
-        }
-      }, 20)
-    })
-    
-    return waitForHoodieToLoad
-  } else {
-    // just return an empty object until we have something to show for this
-    return {}
+  const findTemplate = function (resolve, reject) {
+    console.log('Template ID: ' + templateId)
+    // look through the DB for all the templates
+    hoodie.store.find(templateId)
+      .then((templateDoc) => {
+        console.log('done loading template')
+        // update the store with the list of available templates
+        commit('ACTIVE_TEMPLATE', templateDoc)
+        resolve(templateDoc)
+      })
+      .catch((error) => {
+        console.log(error)
+        reject(error)
+      })
   }
+          
+  return runHoodieFn(commit, state, findTemplate)
 }
 
 export const increment = ({ commit }) => commit('INCREMENT')
